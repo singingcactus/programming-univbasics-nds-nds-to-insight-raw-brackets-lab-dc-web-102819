25c50da9e9ef403e91096c6204d44c7da73df2cb
Matt Bechtel <bechma02@gmail.com>
2019-10-21 16:26:19 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,9 +5,6 @@ require "pp"
 pp directors_database
 
 def directors_totals(nds)
-
-
-
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
   #
@@ -26,5 +23,3 @@ def directors_totals(nds)
   # Be sure to return the result at the end!
   nil
 end
-
-directors_totals
